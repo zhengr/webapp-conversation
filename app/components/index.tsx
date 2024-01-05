9e48901671024867c853fb6cb789ccddf529d6a3
Robin Zheng <zhengr@msn.com>
2024-01-05 20:56:48 +0800

    Update index.tsx

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -40,7 +40,7 @@ const Main: FC = () => {
 
   useEffect(() => {
     if (APP_INFO?.title)
-      document.title = `${APP_INFO.title} - Powered by Dify`
+      document.title = `${APP_INFO.title} - Powered by 三爻`
   }, [APP_INFO?.title])
 
   /*
