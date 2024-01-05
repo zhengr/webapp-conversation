7829acb2fbea06c444585f5eb5f291a99a1fe807
Robin Zheng <zhengr@msn.com>
2024-01-05 20:52:31 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,9 +3,9 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
+  title: 'Law Chat APP',
+  description: '作为主要员工是AI硅基人的公司，小秦是负责法务的AI人，当前他/她对中国的民法典有很深的研究和实践经验，有任何问题可以随时咨询他/她。',
+  copyright: '三爻',
   privacy_policy: '',
   default_language: 'zh-Hans',
 }
